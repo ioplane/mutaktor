e6d0dff3e95b1dcb2391903e4bd15862d866e43c
Pavel Lavrukhin <46395539+dantte-lp@users.noreply.github.com>
2026-03-21 23:16:27 +0300

    fix: add foojay-resolver for JDK auto-download (fixes Qodana + CI toolchain resolution)

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,6 +2,15 @@ rootProject.name = "mutaktor"
 
 pluginManagement {
     includeBuild("build-logic")
+    repositories {
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
+
+plugins {
+    // Auto-download JDKs when not available locally (Qodana, CI, first-time setup)
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.9.0"
 }
 
 include("mutaktor-gradle-plugin")
